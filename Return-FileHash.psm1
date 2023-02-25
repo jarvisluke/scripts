a98c3678d80433ac13e9395ea417c7b06a3a27e7
jarvisluke <123421032+jarvisluke@users.noreply.github.com>
2023-02-24 20:47:31 -0500

    Add files via upload

---
+++ Return-FileHash.psm1
@@ -0,0 +1,15 @@
+﻿function Return-FileHash { 
+    param ( 
+    [Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)]
+    [ValidateSet("SHA1","SHA256","SHA384","SHA512","MD5")] [STRING[]] $hashAlgorithm,
+    [Parameter(Position=1, Mandatory=$true,ValueFromPipeline=$true)] $filepath,
+    [Parameter(Position=2,Mandatory=$true,ValueFromPipeline=$true)] $hashOutput 
+    )
+    New-Item $hashOutput
+    $hashAlgorithm | ForEach-Object -Process {
+        Get-FileHash $filepath -Algorithm $_ | Add-Content $hashOutput
+    }
+
+}
+
+Return-FileHash 
---
+++ pspackageinfo.psm1
@@ -0,0 +1,18 @@
+﻿# Returns results of Get-Package
+function Get-PackageInfo {
+
+	Write-Host "Finding all packages installed with PackageManagement
+	return Get-Package
+	Write-Host "Finding all package providers with PackageManagement
+	Get-PackageProvider
+
+}
+
+
+# Returns results of Get-PackageProvider
+function GetPSInfo {
+
+	Write-Host "Writing output of env variable"
+	Write-Host $env
+
+}
